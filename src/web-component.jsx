269dd119f481f99b04ccfae0288419b75b089d7a
MarselDokubo <connectmarsel@gmail.com>
2024-10-02 13:35:25 +0100

    Developed widget

---
+++ src/web-component.jsx
@@ -0,0 +1,29 @@
+import ReactDom from "react-dom/client";
+import { Widget } from "./components/widget";
+
+export const normalizeAttribute = (att) => {
+  return att.replace(/-([a-z])/g, (_, letter) => letter.toUpperCase());
+};
+
+class WidgetWebComponent extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    const props = this.getPropsFromAttribute();
+    const root = ReactDom.createRoot(this.shadowRoot);
+    root.render(<Widget {...props} />);
+  }
+  getPropsFromAttribute() {
+    const props = {};
+    for (const { name, value } of this.attributes) {
+      props[normalizeAttribute(name)] = value;
+    }
+
+    return props;
+  }
+}
+
+export default WidgetWebComponent;
